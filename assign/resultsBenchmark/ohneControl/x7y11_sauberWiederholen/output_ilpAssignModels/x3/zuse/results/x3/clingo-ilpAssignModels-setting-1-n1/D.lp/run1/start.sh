ca82a92f75eeeb5cf1f1da716a80b6d0c8495603
Luise Werner <luiwerner@uni-potsdam.de>
2019-03-25 11:25:44 +0100

    Add first benchmark results

---
+++ assign/resultsBenchmark/ohneControl/x7y11_sauberWiederholen/output_ilpAssignModels/x3/zuse/results/x3/clingo-ilpAssignModels-setting-1-n1/D.lp/run1/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# http://www.cril.univ-artois.fr/~roussel/runsolver/
+
+CAT="../../../../../../../../programs/gcat.sh"
+
+cd "$(dirname $0)"
+
+#top -n 1 -b > top.txt
+
+[[ -e .finished ]] || "../../../../../../../../programs/runsolver-3.3.7beta" \
+	-M 20000 \
+	-w runsolver.watcher \
+	-o runsolver.solver \
+	-W 30 \
+	"../../../../../../../../programs/clingo-ilpAssignModels" ../../../../../../../../benchmarks/assign/assigned/D.lp '--stats 1 --out-atomf=%s.'
+
+touch .finished
