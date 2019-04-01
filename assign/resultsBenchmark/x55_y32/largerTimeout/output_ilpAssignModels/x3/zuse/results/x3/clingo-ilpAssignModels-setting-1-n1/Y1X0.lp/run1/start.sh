96a57b03b708b4e7c26ef3f187c73acd8b382717
Luise Werner <luiwerner@uni-potsdam.de>
2019-04-01 13:10:17 +0200

    Add new benchmark results

---
+++ assign/resultsBenchmark/x55_y32/largerTimeout/output_ilpAssignModels/x3/zuse/results/x3/clingo-ilpAssignModels-setting-1-n1/Y1X0.lp/run1/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# http://www.cril.univ-artois.fr/~roussel/runsolver/
+
+CAT="../../../../../../../../programs/gcat.sh"
+
+cd "$(dirname $0)"
+
+#top -n 1 -b > top.txt
+
+[[ -e .finished ]] || "../../../../../../../../programs/runsolver-3.3.7beta" \
+	-M 20000 \
+	-w runsolver.watcher \
+	-o runsolver.solver \
+	-W 36000 \
+	"../../../../../../../../programs/clingo-ilpAssignModels" ../../../../../../../../benchmarks/assign/assigned/Y1X0.lp '--stats 1 --out-atomf=%s.'
+
+touch .finished
