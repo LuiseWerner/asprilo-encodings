cda987e1cafe242589aefdf67f52afb567118f5b
Luise Werner <luiwerner@uni-potsdam.de>
2019-04-02 12:24:46 +0200

    Add new results

---
+++ assign/resultsBenchmark/x22_y28/outputs/output_lpAssignModels/x3/zuse/results/x3/clingo-lpAssignModels-setting-1-n1/Simple.lp/run1/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# http://www.cril.univ-artois.fr/~roussel/runsolver/
+
+CAT="../../../../../../../../programs/gcat.sh"
+
+cd "$(dirname $0)"
+
+#top -n 1 -b > top.txt
+
+[[ -e .finished ]] || "../../../../../../../../programs/runsolver-3.3.7beta" \
+	-M 20000 \
+	-w runsolver.watcher \
+	-o runsolver.solver \
+	-W 300 \
+	"../../../../../../../../programs/clingo-lpAssignModels" ../../../../../../../../benchmarks/assign/assigned/Simple.lp '--stats 1 --out-atomf=%s.'
+
+touch .finished
